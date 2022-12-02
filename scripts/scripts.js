183c28e5f668499d3296e04fa400cf6ca4b4ef89
Julien Ramboz <ramboz@adobe.com>
2022-12-02 08:13:39 +0100

    chore: disable performance logger

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,9 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import performanceLogger from './performance.js';
-
-performanceLogger();
 
 /**
  * log RUM if part of the sample.
