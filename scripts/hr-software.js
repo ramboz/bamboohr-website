1c8ef32af28dd4950a61887e688af34ac8d27f3c
David Danielson <ddanielson@bamboohr.com>
2023-04-05 00:43:18 -0600

    building faq schema

--- scripts/hr-software.js
+++ scripts/hr-software.js
@@ -62,41 +62,16 @@ function createFaqPageSchemaMarkup() {
   const faqPageSchema = {
     '@context': 'https://schema.org',
     '@type': 'FAQPage',
-    'mainEntity': [
-      // {
-      //   "@type": "Question",
-      //   "name": "Can I hire using the BambooHR® Mobile app?",
-      //   "acceptedAnswer": {
-      //     "@type": "Answer",
-      //     "text": "You can hire and recruit with BambooHR on your smartphone using the BambooHR® Hiring app (available for iOS and Android). The app is designed specifically for anyone involved in the hiring process. You can use it to review candidates the instant they apply, coordinate with other hiring team members, and communicate directly with applicants. (The BambooHR Hiring app is separate from the BambooHR Mobile app, which is designed for managers and employees.)"
-      //   }
-      // },
-      // {
-      //   "@type": "Question",
-      //   "name": "Which job boards do you integrate with?",
-      //   "acceptedAnswer": {
-      //     "@type": "Answer",
-      //     "text": "Once you create a job posting in BambooHR, it just takes a couple of clicks to share it on Indeed, Glassdoor, ZipRecruiter (coming summer 2021), Facebook Jobs (also coming summer 2021), and social media sites like LinkedIn, Facebook, and Twitter."
-      //   }
-      // },
-      // {
-      //   "@type": "Question",
-      //   "name": "Can I send offer letters to be signed electronically?",
-      //   "acceptedAnswer": {
-      //     "@type": "Answer",
-      //     "text": "Yes! With BambooHR, you can create professional offer letters and send them to new hires to sign electronically, along with any other documents or disclosures you need from them."
-      //   }
-      // },
-      // {
-      //   "@type": "Question",
-      //   "name": "What happens when someone accepts my offer letter?",
-      //   "acceptedAnswer": {
-      //     "@type": "Answer",
-      //     "text": "BambooHR helps you move seamlessly from hiring to onboarding with customizable new hire packets: let new hires know what to expect on their first day, introduce them to their new team, and get important paperwork signed electronically even before their start date."
-      //   }
-      // }
-    ],
+    mainEntity: [],
   }
+
+  document.querySelectorAll('.faq-page-schema .accordion').forEach((tab) => {
+    console.log(tab, ' this is a tab');
+      // const q = tab.querySelector('h2').innerText;
+    // const a = tab.querySelector('p').innerText;
+    // console.log(q, ' question');
+    // console.log(a);
+  });
   const $faqPageSchema = document.createElement('script', { type: 'application/ld+json' });
   $faqPageSchema.innerHTML = JSON.stringify(faqPageSchema);
   const $head = document.head;
@@ -116,7 +91,7 @@ export default async function decorateTemplate() {
     }
     if (val.indexOf('FAQPage')) {
       createFaqPageSchemaMarkup();
-      console.log('This is the FAQ Page Schema.');
+      // console.log('This is the FAQ Page Schema.');
     }
   });
 }
