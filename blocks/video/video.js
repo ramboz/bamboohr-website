2fdce3b8f2354e2a1a56e9d98be0a6c138a903b7
Scott Clayton <sclayton@bamboohr.com>
2023-04-11 11:31:28 -0600

    Update video.js
    Use intersection observer

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,4 +1,8 @@
-export default async function decorate(block) {
+const loadVideo = (block) => {
+  if (block.classList.contains('is-loaded')) {
+    return;
+  }
+
   const anchor = block.querySelector('a');
 
   if (anchor?.href?.endsWith('.mp4')) {
@@ -6,5 +10,16 @@ export default async function decorate(block) {
       <video autoplay muted loop>
         <source src="${anchor.innerText}" type="video/mp4">
       </video>`;
+
+      block.classList.add('is-loaded');
   }
+};
+
+export default async function decorate(block) {
+  const observer = new IntersectionObserver((entries) => {
+    if (entries.some((e) => e.isIntersecting)) {
+      loadVideo(block);
+    }
+  });
+  observer.observe(block);
 }
