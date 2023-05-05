b855eae2f32c01ab819c9611296c870431c7de94
Meng Tian <mtian@bamboohr.com>
2023-05-05 09:14:20 -0600

    mt finish company stats block

--- blocks/company-stats/company-stats.js
+++ blocks/company-stats/company-stats.js
@@ -1,6 +1,24 @@
-import { readBlockConfig, fetchPlaceholders } from "../../scripts/scripts.js";
+import { readBlockConfig, readIndex } from "../../scripts/scripts.js";
 
 export default async function decorate(block) {
   const config = readBlockConfig(block);
-  console.log(config);
+  const options = config.options.split(', ');
+
+  if(options) {
+    block.innerHTML = '<div class="company-stats-list"></div>';
+    const companyStatsElement = block.querySelector('.company-stats-list');
+    await readIndex('/company-stats.json', 'companyStats');
+    const companyStats = window.pageIndex.companyStats.data;
+    
+    options.forEach((option) => {
+      const statData = companyStats.filter(row => row.key === option)[0];
+      const stat = document.createElement('div');
+      stat.className = 'company-stats-list-item';
+      stat.innerHTML = `
+      <span class="icon icon-${statData.icon}"></span>
+      <div class="typ-section-header theme-shade-5">${statData.text}</div>
+      <div class="typ-medium-info theme-shade-5">${statData.name}</div>`;
+      companyStatsElement.append(stat);
+    });
+  }
 }
