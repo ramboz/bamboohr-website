207ea198d959f7d4c7ab8f2cf675b404af9cdb09
Meng Tian <mtian@bamboohr.com>
2023-05-12 14:33:28 -0600

    mt update company stats number to add comma to separate numbers

--- blocks/company-stats/company-stats.js
+++ blocks/company-stats/company-stats.js
@@ -1,8 +1,13 @@
 import { readBlockConfig, readIndex } from "../../scripts/scripts.js";
 
+function isNumber(str) {
+  return !Number.isNaN(Number(str));
+}
+
 export default async function decorate(block) {
   const config = readBlockConfig(block);
-  const options = config.options.split(', ');
+  let options = config.options.split(',');
+  options = options.map((element) => element.trim());
 
   if(options) {
     block.innerHTML = '<div class="company-stats-list"></div>';
@@ -12,11 +17,14 @@ export default async function decorate(block) {
     
     options.forEach((option) => {
       const statData = companyStats.filter(row => row.key === option)[0];
+      let statDataText = statData.text;
+      if (isNumber(statDataText)) statDataText = parseFloat(statDataText).toLocaleString();
+
       const stat = document.createElement('div');
       stat.className = 'company-stats-list-item';
       stat.innerHTML = `
       <span class="icon icon-${statData.icon}"></span>
-      <div class="typ-section-header theme-shade-5">${statData.text}</div>
+      <div class="typ-section-header theme-shade-5">${statDataText}</div>
       <div class="typ-medium-info theme-shade-5">${statData.name}</div>`;
       companyStatsElement.append(stat);
     });
