991fb3aad53f4ef4ca018f3facd2da40f73922d4
David Danielson <ddanielson@bamboohr.com>
2023-05-23 16:45:04 -0600

    Adding expansion product function

--- scripts/content-library.js
+++ scripts/content-library.js
@@ -107,6 +107,20 @@ function downloadPdf() {
   });
 }
 
+function addExpansionProduct(main) {
+  const expansionProduct = getMetadata('expansion-product');
+  console.log(expansionProduct, 'this is the expansion product');
+  const formOnPage = document.querySelectorAll('#mktoForm_3119');
+  console.log(formOnPage, 'this is the form');
+  if (expansionProduct) {
+    // Create a hidden input element
+    const hiddenInput = main.createElement("input");
+    hiddenInput.type = "hidden";
+    hiddenInput.value = expansionProduct;
+    // Append the hidden input to the document body or any other desired element
+    main.formOnPage.appendChild(hiddenInput);
+  }
+}
 
 export default async function decorateTemplate(main) {
   const queryString = window.location.search;
@@ -120,6 +134,7 @@ export default async function decorateTemplate(main) {
     buildSuccessDownload(main, category);
     buildSuccessMoreTitle(main);
     buildSuccessMore(main);
+    addExpansionProduct(main);
     if (category !== 'videos') {
       window.onload = downloadPdf();
     }
