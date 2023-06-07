35e1388f5ce57aee645ff5044ffc301990a8bc1e
David Danielson <ddanielson@bamboohr.com>
2023-06-07 12:28:12 -0600

    breadcrumb work

--- scripts/blog.js
+++ scripts/blog.js
@@ -32,13 +32,12 @@ function buildArticleHeader(main) {
       const section = document.createElement('div');
       section.append(buildBlock('article-header', [
         [picture],
+        [`<p>${category}</p><p>${readtime}</p>`],
         [`<ul>
-            <li><a href="/blog/">Blog ></a></li>
+            <li><a href="/blog/">Blog</a></li>
             <li><a href="/blog/">${category}</a></li>
-            <li><a href="/blog/">${h1}</a></li>
           </ul>`
         ],
-        [`<p>${category}</p><p>${readtime}</p>`],
         [h1],
         [`<p>${author}</p><p>${publicationDate}</p><p>${updatedDate}</p>`],
       ]));
