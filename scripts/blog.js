fe1efa781042f0adc3517020956042e64b52d2de
Meng Tian <mtian@bamboohr.com>
2023-06-20 14:25:58 -0600

    mt update style for blog redesign

--- scripts/blog.js
+++ scripts/blog.js
@@ -26,6 +26,7 @@ function buildImageBlocks(main) {
 function buildArticleHeader(main) {
   try {
     const author = getMetadata('author');
+    const authorPath = author ? `/blog/author/${toClassName(author)}` : '';
     const publicationDate = getMetadata('publication-date');
     const updatedDate = getMetadata('updated-date') || '';
     const readtime = getMetadata('read-time');
@@ -38,6 +39,7 @@ function buildArticleHeader(main) {
       const section = document.createElement('div');
 
       if (testVariation === 'blog-redesign') {
+        const date = updatedDate || publicationDate;
         const categoryItems = category.split(',');
         const categories = categoryItems.map(cat =>
           `<a href="/blog/category/${toSlug(cat.trim())}">${cat}</a>`).join('');
@@ -52,7 +54,7 @@ function buildArticleHeader(main) {
         const articleHeaderBlock = buildBlock('article-header', [
           [breadcrumb],
           [h1],
-          [`<p>${author}</p><p>${publicationDate}</p><p>${updatedDate}</p>`],
+          [`<span>By <a href="${authorPath}">${author}</a></span><span>Updated on ${date}</span><span>${readtime} read</span>`],
           [picture],
         ])
         main.querySelector('main>div').prepend(articleHeaderBlock);
