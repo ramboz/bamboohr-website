49aae9df79a4a6f1da35fa974fbfec2f408bedec
Daniel Oliva <olivaf.daniel@gmail.com>
2023-07-06 09:43:09 -0600

    updates awards block logic and styling

--- blocks/awards/awards.js
+++ blocks/awards/awards.js
@@ -1,5 +1,11 @@
 export default async function decorate(block) {
-  const response = await fetch(`/assets/awards/images.plain.html`);
+  let imagesUrl = '';
+  if (block.classList.contains('variant-1')) {
+    imagesUrl = `/assets/awards/images-variant-1.plain.html`;
+  } else {
+    imagesUrl = `/assets/awards/images.plain.html`;
+  }
+  const response = await fetch(imagesUrl);
   const text = await response.text();
   const DOM = new DOMParser().parseFromString(text, 'text/html');
   const pictures = DOM.querySelectorAll('picture');
