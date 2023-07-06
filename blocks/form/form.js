6e3270a4f458fbb797f434972e2b8a77e897170f
Ralph Trocchia <rtrocchia@bamboohr.com>
2023-07-06 11:54:53 -0400

    include md5 lib, add unique form submission hash to hidden marketo field and adobe submissions

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -549,6 +549,10 @@ export function adobeEventTracking(event, componentData) {
   });
 }
 
+function getUniqueFormSubmissionHash(ecid) {
+  return md5(ecid + Date.now());
+}
+
 function getMktoSearchParams(url) {
   const link = new URL(url);
   const requestType = link.searchParams?.get('requestType');
@@ -705,9 +709,15 @@ function loadFormAndChilipiper(formId, successUrl, chilipiper, floatingLable = f
           .then((result) => {
             // eslint-disable-next-line
             formEl.querySelector('input[name="ECID"]').value = result.identity.ECID;
+			if(formEl.querySelector('input[name="UniqueSubmissionHash"]')){
+			  formEl.querySelector('input[name="UniqueSubmissionHash"]').value = getUniqueFormSubmissionHash(result.identity.ECID);			  
+			}
           })
           .catch( () => { 
             formEl.querySelector('input[name="ECID"]').value = '';
+			if(formEl.querySelector('input[name="UniqueSubmissionHash"]')){
+              formEl.querySelector('input[name="UniqueSubmissionHash"]').value = '';
+			}
           });
           analyticsTrackFormStart(formEl);
         });
