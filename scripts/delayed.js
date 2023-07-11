f28d9056dc29a91efbcacb3c78c78b95e9d20e7f
Scott Clayton <sclayton@bamboohr.com>
2023-07-11 16:47:19 -0600

    ZZ-2248
    SF Chat Test 5--back to the original green button. The one that works consistently.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -166,6 +166,7 @@ function loadSalesforceChatScript2() {
 	}`);
 }
 
+// eslint-disable-next-line no-unused-vars
 function loadSalesforceChatScript3() {
   const chatTestPaths = [
     '/drafts/sclayton/chat-test',
@@ -350,9 +351,9 @@ loadTrustArcFormScript();
 // eslint-disable-next-line
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-ZLCX');
 
-// loadSalesforceChatScript();
+loadSalesforceChatScript();
 // loadSalesforceChatScript2();
-loadSalesforceChatScript3();
+// loadSalesforceChatScript3();
 
 updateExternalLinks();
 
