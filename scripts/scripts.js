b6cd6db0a130f32ad4be1a5770fae444bed03185
Greg Roberts <gregorysroberts@gmail.com>
2023-08-08 14:42:17 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1342,7 +1342,7 @@ async function loadMartech() {
   if ($isOnTestPath) {
 
     /* Adobe Target Prehiding Snippet */
-    //eslint-disable-next-line
+    // eslint-disable-next-line
     ;(function (win, doc, style, timeout) {
       const STYLE_ID = 'alloy-prehiding';
 
